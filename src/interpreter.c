9690d51c0c778cf0f38c7b263b4572066c9453b3
Ross Edwards <redwards64@hotmail.com>
2022-10-05 23:46:56 +0100

    implement basic opcodes

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,4 +1,6 @@
 #include "interpreter.h"
+#include <stdint.h>
+#include <stdio.h>
 
 
 #define WIDTH   64
@@ -19,7 +21,7 @@ uint8_t  sp =                      0x0;
 /* REGISTERS */
 uint8_t  v[16] =                  { 0 };    // registers go from v0 to vf, do not use vf
 uint16_t pc =                     0x200;    // program memory starts at 0x200
-uint16_t instr =                  0x0;      // stores memory addresses
+uint16_t I =                      0x0;      // stores memory addresses
 
 /* TIMERS */
 uint8_t dt =                      0x0;
@@ -47,15 +49,16 @@ uint8_t fontset[80] =             { 0xF0, 0x90, 0x90, 0x90, 0xF0,   // 0
 // set up interpreter state, set memory and registers to zero
 void init(FILE *buffer)
 {
-    for(uint8_t i = 0x50; i <= 0x9F; ++i)
-        mem[i] = fontset[i];
+    for(uint8_t i = 0x0, j = 0x50; j <= 0x9F; ++i, ++j) {
+        mem[j] = fontset[i];
+    }
 
     uint16_t i = 0x200;
     uint16_t c;
-    while((c = fgetc(buffer)) != feof(buffer))
-    {
-        mem[i] = c;
-        ++i;
+    while(fread(&c, sizeof(uint16_t), 1, buffer)) {
+        mem[i] = c & 0xFF;
+        mem[i + 1] = (c >> 8) & 0xFF;
+        i += 2;
     }
 }
 
@@ -73,77 +76,108 @@ void parse_opcode()
     * 7XNN ADD (v_x, address)
     * ANNN LD I (address)
     * DXYN DRW  (v_x, v_y, address) */
-    uint16_t cur_opcode = mem[pc] << 8 | mem[pc + 1];
+    uint16_t cur_opcode = (mem[pc] << 8) & 0xFF00 | (mem[pc + 1]) & 0x00FF;
+    printf("Got opcode %X\n", cur_opcode);
+
+    /* strip bitfields */
+    uint8_t p    = (cur_opcode >> 12) & 0xF; /* 0xF000 */
+    printf("instruction: %X\n", p);
+    uint8_t y    = (cur_opcode >> 8) & 0xF; /* 0x0F00 */
+    printf("v_y: register[%X]\n", y);
+    uint8_t x    = (cur_opcode >> 4) & 0xF; /* 0x00F0 */
+    printf("v_x: register[%X]\n", x);
+    uint8_t kk   = (cur_opcode) & 0xFF;     /* 0x00FF */
+    printf("kk: %X\n", kk);
+    uint16_t nnn = (cur_opcode) & 0xFFF;    /* 0x0FFF */
+    printf("nnn: %X\n", nnn);
+    uint8_t n    = (cur_opcode) & 0xF;
+
     /* mask off the first number of the opcode */
-    switch(cur_opcode & 0xF000) {
-        case 0x0000:
-            switch(cur_opcode & 0x0F00) {
-                case 0x0E00:
-                    switch(cur_opcode & 0x00F) {
-                        case 0x0E0:
+    switch(p) {
+        case 0x0:
+            switch(y) {
+                case 0x0:
+                    switch(kk) {
+                        case 0xE0:
+                            printf("CLEAR SCREEN\n");
                             for(uint16_t i = 0x0; i <= (WIDTH * HEIGHT); ++i) /* CLS */
                                 display[i] = 0x0;
                             pc += 0x2;
                             break;
-                        case 0x00EE:
+                        case 0xEE:
+                            printf("RETURN\n");
                             /*
                             ** RET: set the program counter to the address at the top of the stack and decrement the stack pointer
                              */
                             break;
                     } break;
             } break;
-        case 0x1000:
-            pc = (cur_opcode & 0x0FFF); /* JMP */
+        case 0x1:
+            printf("JUMP TO %X\n", nnn);
+            pc = nnn; /* JMP */
             break;
-        case 0x2000:
+        case 0x2:
 
             break;
-        case 0x3000:
+        case 0x3:
 
             break;
-        case 0x4000:
+        case 0x4:
 
             break;
-        case 0x5000:
+        case 0x5:
 
             break;
-        case 0x6000:
+        case 0x6:
+            printf("LOAD %X IN REGISTER %X\n", kk, x);
             v[(cur_opcode & 0x0F00) >> 8] = cur_opcode & 0x00FF; /* LD */
             pc += 0x2;
             break;
-        case 0x7000:
+        case 0x7:
+            printf("ADD %X TO REGISTER %X\n", kk, x);
             v[(cur_opcode & 0x0F00) >> 8] += cur_opcode & 0x00FF; /* ADD */
             pc += 0x2;
             break;
-        case 0x8000:
+        case 0x8:
 
             break;
-        case 0x9000:
+        case 0x9:
 
             break;
-        case 0xA000:
-            instr = (cur_opcode & 0x0FFF) + v[0]; /* LD I */
+        case 0xA:
+            printf("LOAD INSTRUCTION %X\n", nnn);
+            I = (cur_opcode & 0x0FFF) + v[0]; /* LD I */
             pc += 0x2;
             break;
-        case 0xB000:
+        case 0xB:
 
             break;
-        case 0xC000:
+        case 0xC:
 
             break;
-        case 0xD000:
+        case 0xD:
+        {
+            printf("DRAW %X BYTES TO REGISTERS %X AND %X\n", n, y, x);
+            uint16_t sprites[n];
+            uint16_t addr = I;
+            for(uint8_t i = 0; i < n; i++) {
+                sprites[i] = addr;
+                addr++;
+                display[v[x + i] * v[y + i]] ^= sprites[i];
+            }
+        }
+            break;
+        case 0xE:
 
             break;
-        case 0xE000:
+        case 0xF:
 
             break;
-        case 0xF000:
-
+        default:
+            printf("Unknown opcode: %X\n", cur_opcode);
             break;
     }
 }
-        /* case 0x00E0: */
-        /*     break; */
 
 /* OPCODE FUNCTIONS */
 void cls()
--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 
 #include "interpreter.h"
+#include <stdio.h>
 
 
 int main(int argc, char **argv)
 {
-    FILE *file = fopen(argv[0], "rb");
+    FILE *file = fopen("programs/IBM Logo.ch8", "rb");
+    if (file == NULL) {
+        printf("coulnt open file\n");
+    }
     uint16_t opcode;
 
     init(file);
-    while(1) {
+    for(int i = 0; i < 20; i++) {
         /* FETCH */
 
         /* DECODE */
@@ -18,4 +22,5 @@ int main(int argc, char **argv)
     }
 
     fclose(file);
+    return 0;
 }
